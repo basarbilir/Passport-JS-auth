31a237c5cb129bf28fcfd0971d96e2b155f25aa9
basarbilir <bilirbasar@gmail.com>
2020-05-19 20:51:39 +0300

    deploy all project files

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Passport-JS-auth
-Use passport js and google authentication to log in
+Use passport js and google authentication to log in,
+Before you start running fill the neccessary information on "config/key.js",
+then write "npm install" and "node app" to run
---
+++ config/keys.js
@@ -0,0 +1,14 @@
+// add this file to .gitignore
+
+module.exports = {
+    google: {
+        clientID: 'writeYourClientID',
+        clientSecret:'writeYourClientSecret'
+    },
+    mongodb: {
+        dbURI: 'mongodb://localhost/oauth-test'
+    },
+    session: {
+        cookieKey: "heisamagneficentlearner"
+    }
+};
